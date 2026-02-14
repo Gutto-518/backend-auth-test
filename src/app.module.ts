644054df5f46b7d74f59149d657f9fb0a2a4aa03
Andres <andres@518.rent>
2026-02-14 09:55:12 -0400

    cambios en el module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,8 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { HelloModule } from './hello/hello.module';
 import { FirebaseModule } from './firebase/firebase.module';
-
 @Module({
-  imports: [AuthModule, HelloModule, FirebaseModule],
+  imports: [AuthModule, HelloModule, FirebaseModule ],
   controllers: [AppController],
   providers: [AppService],
 })
